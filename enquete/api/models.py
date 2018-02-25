3bf5c3346d385fc0f29d200aa45a2b073c4eb940
AlexandreLima <xandebre@gmail.com>
2018-02-25 17:51:41 -0300

    inclusão dos modelos básicos

--- enquete/api/models.py
+++ enquete/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Enquete(models.Model):
+    
+    
+
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
+
+
+class Voto(models.Model):
+    
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
