f54b44f3eae49710ef54d7771daad1e9908fe78b
AlexandreLima <xandebre@gmail.com>
2018-02-25 17:05:56 -0300

    Inclusão do projeto básico funcional

--- enquete/api/views.py
+++ enquete/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+from api.serializer import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
