b694f251365e8744ccf2df58cde907dc6aa028c1
lucasavs <lucasavs@id.uff.br>
2018-02-28 11:51:12 -0300

    Modelo da enquente(survey) e das opções(Option)

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Survey(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Option(models.Model):
+    description = models.CharField(max_length=30)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    position = models.IntegerField
+
+    def __str__(self):
+        return "%s - %s " % (self.survey.name, self.description)
+
+    class Meta:
+        ordering = 'position'  # talvez tenha que colocar entre parenteses, como ('position')
