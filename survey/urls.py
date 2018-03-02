6adc582e9c851e60c1e1cb10e0c02ed1fd62c168
lucasavs <lucasavs@id.uff.br>
2018-03-02 10:47:49 -0300

    Separando as requisições entre privadas e públicas

--- survey/urls.py
+++ survey/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import SurveyInfo, OptionInfo
+from .views import SurveyPrivate, OptionPublic
 
 urlpatterns = [
      # lista todas as enquetes (GET) OU cria uma nova enquete (POST)
-     path('survey/', SurveyInfo.as_view(), {'pk': None}),
+     path('survey/', SurveyPrivate.as_view(), {'pk': None}),
      # Mostra uma enquete específica (GET), modifica uma enquete (PUT), remove uma enquete (DELETE)
-     path('survey/<int:pk>', SurveyInfo.as_view()),
-     path('option/', OptionInfo.as_view())
+     path('survey/<int:pk>', SurveyPrivate.as_view()),
+     path('option/', OptionPublic.as_view())
 
 ]
--- survey/views.py
+++ survey/views.py
@@ -8,7 +8,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 
 
-class SurveyInfo(APIView):
+class SurveyPrivate(APIView):
     # Usando o BasicAuthentication para fazer a autenticação sem entrar em mais detalhes.
     # Para usar esse código em produção, recomenda-se usar a autenticação via token
     # Se quiser mesmo usar a BasicAuthentication em produção, ao menos utilize HTTPS
@@ -37,7 +37,22 @@ class SurveyInfo(APIView):
 
     def post(self, request, pk):
         """
-        Cria uma nova enquete
+        Cria uma nova enquete. Exemplo de enquete criada
+        {
+            "name": "Qual sua comida favorita?",
+            "description": "Escolha sua comida favorita",
+            "options": [
+                {
+                    "description": "Coxinha",
+                    "position": 1
+                },
+                {
+                    "description": "Pizza",
+                    "position": 2
+                }
+            ]
+
+        }
         :param request:
         :return:
         """
@@ -52,11 +67,17 @@ class SurveyInfo(APIView):
     def put(self, request, pk):
         """
         Atualiza os dados de uma nova enquete
+        url: http://localhost:8000/survey/<id da enquete>
+        {
+            "name" : "Lista de sex symbols",
+            "description": "soh coisa linda de deus"
+        }
         :param request:
         :param pk:
         :return:
         """
-        # Tem que colocar o partial = true senão ele tenta validar todos os campos, e campos obrigatórios que não foram passados como parametros irão disparar exceções
+        # Tem que colocar o partial = true senão ele tenta validar todos os campos,
+        # campos obrigatórios que não foram passados como parametros irão disparar exceções
         try:
             survey = Survey.objects.get(pk=pk)
         except ObjectDoesNotExist:
@@ -64,7 +85,8 @@ class SurveyInfo(APIView):
         serializer = self.serializerClass(survey, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+            # você pode passar serializer.data como parametro no response para exibir o objeto criado
+            return Response(status=status.HTTP_204_NO_CONTENT)
         else:
             return Response(serializer.errors, status=status.HTTP_409_CONFLICT)
 
@@ -87,7 +109,7 @@ class SurveyInfo(APIView):
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-class OptionInfo(APIView):
+class OptionPublic(APIView):
     serializerClass = OptionSerializer
 
     def post(self, request):
@@ -105,7 +127,7 @@ class OptionInfo(APIView):
         :return:
         """
         try:
-            pk = request.data["pk"]
+            pk = request.data["id"]
             option = Option.objects.get(pk=pk)
             option.add_vote()
             return Response(status=status.HTTP_204_NO_CONTENT)
@@ -113,3 +135,14 @@ class OptionInfo(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         except RuntimeError:
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class OptionPrivate(APIView):
+    # Usando o BasicAuthentication para fazer a autenticação sem entrar em mais detalhes.
+    # Para usar esse código em produção, recomenda-se usar a autenticação via token
+    # Se quiser mesmo usar a BasicAuthentication em produção, ao menos utilize HTTPS
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+    serializerClass = OptionSerializer
+
+    # def put(self,request):
