6b534e1bfca64d343d024e68a2126dc890db7709
lucasavs <lucasavs@id.uff.br>
2018-03-03 19:02:28 -0300

    teste - dando get em uma enquete especifica

--- survey/tests.py
+++ survey/tests.py
@@ -1,7 +1,8 @@
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase, APIClient, RequestsClient
+from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
+from .models import Survey, Option
 
 
 class GetSurveyTest(APITestCase):
@@ -9,8 +10,15 @@ class GetSurveyTest(APITestCase):
         """Define the test client and other test variables."""
         self.superuser = User.objects.create_superuser('admin', 'admin@admin.com', 'adminadmin')
         self.client.login(username='admin', password='adminadmin')
+        self.survey = Survey.objects.create(name="teste", description="teste")
 
     def test_can_get_surveys(self):
         url = reverse('survey-general')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_get_survey(self):
+        url = reverse('survey-specific', args=[self.survey.id])
+
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
--- survey/urls.py
+++ survey/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
      # lista todas as enquetes (GET) OU cria uma nova enquete (POST)
      path('survey/', SurveyPrivate.as_view(), {'pk': None}, name="survey-general"),
      # Mostra uma enquete específica (GET), modifica uma enquete (PUT), remove uma enquete (DELETE)
-     path('survey/<int:pk>', SurveyPrivate.as_view()),
+     path('survey/<int:pk>', SurveyPrivate.as_view(), name="survey-specific"),
      # faz um unico voto (POST)
      path('vote/', OptionPublic.as_view()),
      # Modifica uma opção (PUT)
