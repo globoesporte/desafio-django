0ea5aa35ac066213d72252403c69df96ab7d209c
lucasavs <lucasavs@id.uff.br>
2018-03-03 18:19:49 -0300

    Automatização de teste: pegar todas as enquetes

--- survey/tests.py
+++ survey/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient, RequestsClient
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class GetSurveyTest(APITestCase):
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.superuser = User.objects.create_superuser('admin', 'admin@admin.com', 'adminadmin')
+        self.client.login(username='admin', password='adminadmin')
+
+    def test_can_get_surveys(self):
+        url = reverse('survey-general')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- survey/urls.py
+++ survey/urls.py
@@ -3,7 +3,7 @@ from .views import SurveyPrivate, OptionPublic, OptionPrivate
 
 urlpatterns = [
      # lista todas as enquetes (GET) OU cria uma nova enquete (POST)
-     path('survey/', SurveyPrivate.as_view(), {'pk': None}),
+     path('survey/', SurveyPrivate.as_view(), {'pk': None}, name="survey-general"),
      # Mostra uma enquete específica (GET), modifica uma enquete (PUT), remove uma enquete (DELETE)
      path('survey/<int:pk>', SurveyPrivate.as_view()),
      # faz um unico voto (POST)
