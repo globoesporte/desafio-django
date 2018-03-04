76d2bf770337c02e25698dcd8c311489a7c42783
Glaucia Esppenchutz <g.esppen@gmail.com>
2018-03-03 21:28:15 -0300

    creating auth in API REST endpoints

--- main/polls/views.py
+++ main/polls/views.py
@@ -4,17 +4,32 @@ from rest_framework import viewsets
 from polls import models
 from . import serializers
 
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 
 class OptionViewSet(viewsets.ModelViewSet):
-	queryset = models.Option.objects.all()
-	serializer_class = serializers.OptionSerializer
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+    queryset = models.Option.objects.all()
+    serializer_class = serializers.OptionSerializer
 
 
 class PollViewSet(viewsets.ModelViewSet):
-	queryset = models.Poll.objects.all()
-	serializer_class = serializers.PollSerializer
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+    queryset = models.Poll.objects.all()
+    serializer_class = serializers.PollSerializer
 
 
 class VoteViewSet(viewsets.ModelViewSet):
-	queryset = models.Vote.objects.all()
-	serializer_class = serializers.VoteSerializer
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    queryset = models.Vote.objects.all()
+    serializer_class = serializers.VoteSerializer
+
+    def get_permissions(self):
+        self.permission_classes = [IsAuthenticated, ]
+        if self.request.method == 'POST':
+            self.permission_classes = []
+
+        return super(VoteViewSet, self).get_permissions()
