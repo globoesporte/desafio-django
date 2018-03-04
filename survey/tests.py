db0aebf125ab251c865e022cc0044eae5ff63ae7
lucasavs <lucasavs@id.uff.br>
2018-03-03 22:03:03 -0300

    teste de criação de enquete

--- survey/tests.py
+++ survey/tests.py
@@ -13,12 +13,30 @@ class GetSurveyTest(APITestCase):
         self.survey = Survey.objects.create(name="teste", description="teste")
 
     def test_can_get_surveys(self):
+        """
+        Checa se consegue pegar a lista de todas as enquetes
+        """
         url = reverse('survey-general')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_can_get_survey(self):
+        """
+        Checa se consegue pegar as informações de uma unica enquete
+        """
         url = reverse('survey-specific', args=[self.survey.id])
 
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class SaveSurveyTest(APITestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_superuser('admin', 'admin@admin.com', 'adminadmin')
+        self.client.login(username='admin', password='adminadmin')
+        self.data = {'name': 'Name', 'description': 'Description'}
+
+    def test_can_create_survey(self):
+        url = reverse('survey-general')
+        response = self.client.post(url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
