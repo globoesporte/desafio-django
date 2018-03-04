6c83ea6027b5dd27bb729264492b0085ea5f4c95
AlexandreLima <xandebre@gmail.com>
2018-03-04 19:46:07 -0300

    Adicionando itens que faltavam no commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alexandre/Documentos/develop/desafio_django_globo_com/desafio-django/env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
