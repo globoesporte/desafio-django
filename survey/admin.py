23310530d1fd69f226634da8c1829a90529969db
lucasavs <lucasavs@id.uff.br>
2018-03-05 18:02:50 -0300

    campos de busca no view de Admin

--- survey/admin.py
+++ survey/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Survey, Option
 
-admin.site.register(Survey)
-admin.site.register(Option)
+
+# admin.site.register(Option)
+
+
+class SurveyAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+class OptionAdmin(admin.ModelAdmin):
+    search_fields = ('description',)
+
+admin.site.register(Survey, SurveyAdmin)
+admin.site.register(Option, OptionAdmin)
