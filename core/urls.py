722718fe359cb017d79e960b6e03f8b016a5efb0
mesmer <mesmer@fisica.if.uff.br>
2018-03-10 22:42:15 +0000

    urls to have mini options

--- core/urls.py
+++ core/urls.py
@@ -4,13 +4,22 @@ from .views import SurveyList, SurveyActs, OptionActs, VoteActs
 
 urlpatterns = [
     path(
+        'api/vote/<int:survey>/<int:pk>',
+        VoteActs.as_view()),
+    path(
         'api/vote/survey=<int:survey>/option=<int:pk>',
+        VoteActs.as_view()),
+    path(
+        'api/vote/<int:survey>/<int:pkold>/<int:pk>',
         VoteActs.as_view()),
     path(
         'api/vote/survey=<int:survey>/old_option=<int:pkold>/new_option=<int:pk>',
         VoteActs.as_view()),
     path(
         'api/options/',
+        OptionActs.as_view()),
+    path(
+        'api/options/<int:survey>/<int:pk>',
         OptionActs.as_view()),
     path(
         'api/options/survey=<int:survey>/option=<int:pk>',
@@ -20,6 +29,9 @@ urlpatterns = [
         SurveyActs.as_view(),
         {
             'pk': None}),
+    path(
+        'api/surveys/survey=<int:pk>',
+        SurveyActs.as_view()),
     path(
         'api/surveys/<int:pk>',
         SurveyActs.as_view()),
