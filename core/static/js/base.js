758c7d50b152d51fe3b79805c05c1c1964a01835
mesmer <mesmer@fisica.if.uff.br>
2018-03-11 02:25:28 +0000

    rework template, for smooth animation

---
+++ core/static/js/base.js
@@ -0,0 +1,51 @@
+$(window).on('load', function() {
+//Linear easing
+function linear (duration, range, current) {
+  return ((duration * 2) / Math.pow(range, 2)) * current;
+}
+function animateValue(id, start, duration, easing) {
+  var end = Number(id.text())
+  var range = end - start;
+  var current = start;
+  var increment = end > start? 1 : -1;
+  var startTime = new Date();
+  var offset = 1;
+  var remainderTime = 0;
+  var step = function() {
+    if (current < end) {
+    current += increment;
+    id.text(current+"%");
+    }
+    if (current != end) {
+      setTimeout(step, easing(duration, range, current));
+    }
+  };
+  
+  setTimeout(step, easing(duration, range, start));
+}
+
+$('.perc').each(function(){
+animateValue($(this), 0, 1000, linear);
+
+});
+
+$("button").click(function(){ 
+    survey= $(this).attr("survey");
+    option= $(this).attr("option");
+    votos= $(this).attr("votos");
+
+$.ajax({
+  url:'/api/vote/survey='+survey+'/option='+option,
+  type:'POST',
+    data: {
+            csrfmiddlewaretoken:"{{ csrf_token }}"
+        },
+});
+    $(this).attr("votos",Number(votos)+1);
+    votos= $(this).attr("votos");
+    $(this).parent().prev("h3").text("votos:"+votos)
+    location.reload();
+ });
+}); 
+
+
