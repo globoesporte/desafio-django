c9f74ffd1fc234f57ff1d3626f270b6e76890b56
renatinho <renatoferreira249@gmail.com>
2018-03-21 07:07:38 -0300

    testes de modelo

--- src/api/tests.py
+++ src/api/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from enqueteapp.models import Choice, Option
+from .views import AllQuestionsView, QuestionView
+
+from rest_framework.test import APIRequestFactory, APITestCase, force_authenticate()
+
+class AllQuestionsAPITest(APITestCase):
+
+    def setUpTestData(cls):
+        user1 = User.objects.create_user('test_user1', password ='xxx')
+        user2 = User.objects.create_user('test_user2', password = 'yyy')
+
+        question = Question.objects.create(
+                question_text="texto", pub_date=datetime(2018,2,1))
+        choice =  Choice.objects.create(question = question, choice_text = 'xxx')
+        client = AllQuestionsView()
+
+    def setUp(self):
+        pass
+
+    def test_get_questions(self):
+
+        
+    def test_post_question(self):
+        pass
+
+    def test_get_question_detail(self):
+        pass
+    
+    def test_put_question(self):
+        pass
+
+    def test_delete_question(self):
+        pass
+
+    def test_get_votes(self):
+        pass
+
+    def test_post_vote(self):
+
+
+
--- src/enqueteapp/tests.py
+++ src/enqueteapp/tests.py
@@ -1,4 +1,43 @@
 from django.test import TestCase
+from .models import Choice, Question
+from datetime import datetime
 
-class Test(TestCase):
+class QuestionTest(TestCase):
 
+    def setUpTestData(cls):
+        question = Question.objects.create(
+                question_text="texto", pub_date=datetime(2018,2,1))
+
+    def test_question_text(self):
+        question = Question.objects.get(id=1)
+        return self.assertEqual(question.question_text, 'texto')
+
+    def test_pub_date(self):
+        question = Question.objects.get(id=1)
+        return self.assertEqual(datetime(2018,2,1), question.pub_date)
+
+    def test_question_txt_max_len(self):
+        question = Question.objects.get(id=1)
+        max_len = question._meta.get_field('question_text').max_length
+        return self.assertEqual(max_len,200)
+
+
+class ChoiceTest(TestCase):
+
+    def setUpTestData(cls):
+        question = Question.objects.create(
+                question_text="texto", pub_date=datetime(2018,2,1))
+        choice =  Choice.objects.create(question = question, choice_text = 'xxx')
+
+    def test_question(self):
+        choice = Choice.object.get(id=1)
+        return self.assertEqual(choice.question.id, 1) 
+
+    def test_choice_text(self):
+        choice = Choice.object.get(id=1)
+        return self.assertEqual(choice.choice_text, 'xxx')
+
+    def test_vote(self):
+        choice = Choice.objects.get(id=1) 
+        choice.add_vote()
+        return self.assertEqual(1, choice.votes)
